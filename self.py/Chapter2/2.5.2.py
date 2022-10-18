184464ad2eb22d0578f5f295cbc244dd2bad587d
dkiexe <116086325+dkiexe@users.noreply.github.com>
2022-10-18 17:48:32 +0300

    Add files via upload

---
+++ self.py/Chapter2/2.5.2.py
@@ -0,0 +1,16 @@
+# Do you also think it's time to start playing?
+# In this exercise you will write the basic action of a player in the game: guessing a letter using a simple input action.
+
+# Write a piece of code in which:
+
+# Show the player a message asking him to type a character (his guess).
+# Enter the character into a variable of your choice (assuming the input is correct).
+# Print the character to the screen.
+
+# Guidelines:
+# Ensure accurate output.
+# Make sure the variable name you define complies with the conventions you have learned.
+
+# My_solution
+guess = input("Guess a letter: ")
+print(guess)
